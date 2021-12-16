7a49ae743fc2e2fb6fb143f2b7a3e4c76121b400
juno <juno7803@naver.com>
2021-12-16 18:54:35 +0900

    feat: reconciliation

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,4 @@
-import { Element } from 'types';
-
-function createElement(type: string, props: any, ...children: Element[]) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === 'object' ? child : createTextElement(child)
-      ),
-    },
-  };
-}
+import { Element, Fiber } from 'types';
 
 function createTextElement(text: string) {
   return {
@@ -22,20 +10,117 @@ function createTextElement(text: string) {
   };
 }
 
-function render(element: Element, container: HTMLElement | Text) {
-  const dom =
-    element.type == 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type);
-  const isProperty = (key: string) => key !== 'children';
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+function createElement(
+  type: string,
+  props: any,
+  ...children: Element[]
+): Element {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  };
 }
+
+function createDom(fiber: Fiber) {
+  const dom =
+    fiber.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type as string);
+
+  const isProperty = (property: string) => property !== 'children';
+
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((prop) => {
+      // @ts-ignore
+      dom[prop] = fiber.props[prop];
+    });
+
+  return dom;
+}
+
+let nextUnitOfWork: Fiber | null = null;
+
+function render(element: Element, container: HTMLElement) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+function performUnitOfWork(fiber: Fiber): Fiber | null {
+  // TODO create element and add to dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    // parent 가 있으면 dom 도 있겠지만 type check 가 안되서 optional chaning을 한다
+    fiber.parent.dom?.appendChild(fiber.dom);
+  }
+
+  // TODO create new fibers for children
+  const childElements = fiber.props.children as Element[];
+  let index = 0;
+  let prevSibling: Fiber | null = null;
+
+  while (index < childElements.length) {
+    const element = childElements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling!.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // TODO return(select) next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber: Fiber | undefined = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+
+  return null;
+}
+
+function workLoop(deadline: IdleDeadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  window.requestIdleCallback(workLoop);
+}
+
+window.requestIdleCallback(workLoop);
 
 const junoReact = {
   createElement,
--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,15 @@ export interface Element {
 }
 
 export interface Props {}
+
+export interface Fiber {
+  type?: string | Function;
+  dom: HTMLElement | Text | null;
+  parent?: Fiber;
+  child?: Fiber;
+  sibling?: Fiber;
+  props: {
+    children: Array<Element | Fiber>;
+    [key: string]: any;
+  };
+}
